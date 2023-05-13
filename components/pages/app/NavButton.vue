fa5341e880b8f8d740cf8afe03194bbd1bfa3bbf
David Docampo <david@docampo.ch>
2023-05-13 19:45:56 +0200

    finish navButton hover effect

--- components/pages/app/NavButton.vue
+++ components/pages/app/NavButton.vue
@@ -1,13 +1,16 @@
 <template>
-  <div class="mx-8 w-1/2">
-    <div class="flex border border-lime-500 hover:bg-lime-500 text-lime-500 hover:text-gray-950 transition-all duration-150">
+  <div class="relative w-1/2 h-full">
+    <div @mouseenter="currentlyHovering = true" @mouseleave="currentlyHovering = false" class="flex border border-lime-500 hover:bg-lime-500 text-lime-500 hover:text-gray-950 transition-all duration-150">
       <NuxtLink :to="to" class="py-4 h-full w-full inline-flex justify-center content-center">{{ text }}</NuxtLink>
     </div>
+    <div class="absolute -z-10 top-0 bottom-0 right-0 left-0 border border-lime-500 transform transition-all duration-150"
+          :class="{'-translate-x-4 -translate-y-4' : currentlyHovering}"></div>
   </div>
 </template>
 
 <script setup>
 const props = defineProps(['text', 'to'])
 
+const currentlyHovering = ref(false)
 
 </script>
--- components/pages/app/TitleNav.vue
+++ components/pages/app/TitleNav.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-2/3 z-50 flex justify-around items-center">
-    <NavButton text="About Me" to="/about" />
-    <NavButton text="Projects" to="/projects" />
+  <div class="w-2/3 z-50 flex justify-between content-center">
+    <NavButton text="About Me" to="/about" class="mr-4" />
+    <NavButton text="Projects" to="/projects" class="ml-4" />
   </div>
 </template>
 
