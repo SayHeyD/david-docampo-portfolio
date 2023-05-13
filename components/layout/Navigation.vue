7e42e82677bc5f204e4a3344222290722d8b1588
David Docampo <david@docampo.ch>
2023-05-13 22:45:50 +0200

    add working to compass button

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -5,7 +5,7 @@
     </div>
 
     <div class="absolute bottom-0 right-0 left-0 flex justify-end">
-      <Compass class="h-12 w-12 m-4 text-green-500 cursor-pointer hover:scale-110 transition-transform duration-150" />
+      <Compass @click="navigateTo('/')" class="h-12 w-12 m-4 text-green-500 cursor-pointer hover:scale-110 transition-transform duration-150" />
     </div>
   </div>
 </template>
