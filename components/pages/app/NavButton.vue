29d1136e96d58f70b871fd8ae026e9f135a4b405
David Docampo <david@docampo.ch>
2023-05-14 00:20:43 +0200

    make button text bigger

--- components/pages/app/NavButton.vue
+++ components/pages/app/NavButton.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="relative w-1/2 h-full">
     <div @mouseenter="currentlyHovering = true" @mouseleave="currentlyHovering = false" class="flex border border-lime-500 hover:bg-lime-500 text-lime-500 hover:text-gray-950 transition-all duration-150">
-      <NuxtLink :to="to" class="py-4 h-full w-full inline-flex justify-center content-center">{{ text }}</NuxtLink>
+      <NuxtLink :to="to" class="py-4 text-xl h-full w-full inline-flex justify-center content-center">{{ text }}</NuxtLink>
     </div>
     <div class="absolute -z-10 top-0 bottom-0 right-0 left-0 border border-lime-500 transform transition-all duration-150"
           :class="{'-translate-x-4 -translate-y-4' : currentlyHovering}"></div>
