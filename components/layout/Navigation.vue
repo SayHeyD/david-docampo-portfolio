131c1d3557625a47f5ac81391ee44c9ab49ecab3
David Docampo <david@docampo.ch>
2023-05-14 23:56:43 +0200

    remove unneccessary newline

--- components/layout/Navigation.vue
+++ components/layout/Navigation.vue
@@ -1,8 +1,6 @@
 <template>
   <div class="fixed top-0 bottom-0 left-0 right-0 z-40 bg-transparent">
-    <div class="absolute top-0 right-0 left-0">
-      
-    </div>
+    <div class="absolute top-0 right-0 left-0"></div>
 
     <div class="absolute bottom-0 right-0 left-0 flex justify-end">
       <Compass @click="navigateTo('/')" class="h-12 w-12 m-4 text-green-500 cursor-pointer hover:scale-110 transition-transform duration-150" />
