a79460c00264d4f9fce768493b65e6b8049b4b7a
David Docampo <david@docampo.ch>
2023-05-15 00:38:33 +0200

    add blur for page transitions

--- app.vue
+++ app.vue
@@ -7,3 +7,15 @@
 <script setup>
 import Layout from '@/components/layout/Layout.vue'
 </script>
+
+<style>
+.page-enter-active,
+.page-leave-active {
+  transition: all 0.4s;
+}
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
+  filter: blur(1rem);
+}
+</style>
