daf9a732166fc2fe8a7e8b48f03a979f7b925c0a
David Docampo <david@docampo.ch>
2023-05-15 00:25:52 +0200

    add slanted arrow icon

---
+++ components/icons/SlantedArrow.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    <svg class="h-full w-full" viewBox="0 0 300 300" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M29.7072 79.313C29.7072 79.313 47.6623 129.57 109.026 160.81C170.391 192.05 262.703 186.365 262.703 186.365M262.703 186.365L212.044 147.794M262.703 186.365L212.044 220.687" stroke="currentColor" stroke-width="4"/>
+    </svg>
+  </div>
+</template>
