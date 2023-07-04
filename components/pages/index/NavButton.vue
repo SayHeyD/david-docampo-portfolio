76b7c76c9a055e1941ea76715b47e69502dec992
David Docampo <david@docampo.ch>
2023-07-04 18:07:38 +0200

    use css grid instead of flexbox to align navbuttons

--- components/pages/index/NavButton.vue
+++ components/pages/index/NavButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="relative w-full md:w-1/2 h-full">
+  <div class="relative w-full h-full">
     <div @mouseenter="currentlyHovering = true" @mouseleave="currentlyHovering = false" class="flex border border-lime-500 hover:bg-lime-500 text-lime-500 hover:text-gray-950 transition-all duration-150">
       <NuxtLink :to="to" :target="target" class="py-4 text-xl h-full w-full inline-flex justify-center content-center">{{ text }}</NuxtLink>
     </div>
--- components/pages/index/TitleNav.vue
+++ components/pages/index/TitleNav.vue
@@ -1,7 +1,8 @@
 <template>
-  <div class="w-full sm:w-2/3 z-50 flex flex-col md:flex-row justify-between content-center">
-    <NavButton text="LinkedIn" to="https://www.linkedin.com/in/david-docampo-47a9581a2/" target="_blank" class="mr-0 mb-6 md:mb-0 md:mr-2" />
-    <NavButton text="GitHub" to="https://github.com/SayHeyD" target="_blank" class="ml-0 mb-6 md:mb-0 md:mr-2" />
+  <div class="w-full sm:w-2/3 z-50 grid grid-cols-1 md:grid-cols-2 gap-6">
+    <NavButton text="LinkedIn" to="https://www.linkedin.com/in/david-docampo-47a9581a2/" target="_blank" class="w-full" />
+    <NavButton text="GitHub" to="https://github.com/SayHeyD" target="_blank" class="w-full" />
+    <NavButton text="Education" to="/education" class="w-full" />
   </div>
 </template>
 
