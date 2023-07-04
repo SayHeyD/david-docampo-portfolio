ccbca23560b8fcb28f7cf83e53052fcde407fa20
David Docampo <david@docampo.ch>
2023-07-04 17:46:19 +0200

    make index page more responsive

--- components/pages/index/TitleNav.vue
+++ components/pages/index/TitleNav.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class="w-full md:w-2/3 z-50 flex justify-between content-center">
-    <NavButton text="LinkedIn" to="https://www.linkedin.com/in/david-docampo-47a9581a2/" target="_blank" class="mr-2 md:mr-2" />
-    <NavButton text="GitHub" to="https://github.com/SayHeyD" target="_blank" class="ml-2 md:mr-2" />
+  <div class="w-full sm:w-2/3 z-50 flex flex-col md:flex-row justify-between content-center">
+    <NavButton text="LinkedIn" to="https://www.linkedin.com/in/david-docampo-47a9581a2/" target="_blank" class="mr-0 mb-6 md:mb-0 md:mr-2" />
+    <NavButton text="GitHub" to="https://github.com/SayHeyD" target="_blank" class="ml-0 mb-6 md:mb-0 md:mr-2" />
   </div>
 </template>
 
--- components/pages/index/TitleText.vue
+++ components/pages/index/TitleText.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="w-full md:w-2/3">
-    <h1 class="text-4xl md:text-5xl lg:text-6xl font-medium pb-3 mb-3 md:pb-6 md:mb-6 border-b-2 md:border-b-4 border-lime-500">David Docampo</h1>
+  <div class="w-full sm:w-2/3">
+    <h1 class="text-4xl lg:text-6xl font-medium pb-3 mb-3 md:pb-6 md:mb-6 border-b-2 md:border-b-4 border-lime-500">David Docampo</h1>
     <p class="text-xl lg:text-3xl">DevOps, Hobby Musician, Gamer</p>
   </div>
 </template>
