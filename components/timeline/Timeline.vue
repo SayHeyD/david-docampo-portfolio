d812f69f060282e678ef473b77da47193f2b8c8d
David Docampo <david@docampo.ch>
2023-07-04 21:50:26 +0200

    add first draft of timeline and timeline entries

---
+++ components/timeline/Timeline.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="w-full z-40">
+        <div id="timeline-layer" class="absolute top-0 bottom-0 right-0 left-0">
+            <div class="relative z-10 ml-24 bg-lime-500 h-full w-1"></div>
+        </div>
+        <div class="absolute top-0 bottom-0 right-0 left-0 mt-20">
+            <TimelineEntry
+                v-for="entry in timelineEntries"
+                :key="entry.id"
+                :title="entry.title"
+                :subtitle="entry.subtitle"
+                :date="entry.date"
+                class="relative mb-10"
+            >
+                <span v-text="entry.text" />
+            </TimelineEntry>
+        </div>
+    </div>
+</template>
+  
+<script setup>
+import TimelineEntry from '@/components/timeline/TimelineEntry'
+
+const props = defineProps({
+    timelineEntries: {
+        type: Array,
+        default: []
+    }
+})
+</script>
---
+++ components/timeline/TimelineEntry.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="relative ml-20 h-48">
+        <div class="absolute top-0 bottom-0 right-0 left-0">
+            <div id="timeline_dot" class="mt-8 translate-x-2.5 rounded-full w-4 h-4 bg-lime-500"></div>
+        </div>
+        <div class="absolute top-0 bottom-0 right-0 left-0 ml-24 w-4/5">
+            <h3 class="text-3xl font-bold mb" v-text="title" />
+            <h4 class="text-xl mb-4"><span v-text="subtitle" /> | <span v-text="date" /></h4>
+            <div class="text-lg">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script setup>
+
+const props = defineProps({
+    title: {
+        type: String,
+        required: true,
+    },
+    subtitle: {
+        type: String,
+        default: '',
+    },
+    date: {
+        type: String,
+        required: true,
+    },
+    link: {
+        type: String,
+        required: true,
+    }
+})
+</script>
