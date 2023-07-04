ff6b09c9d78c3972428d0d7c437cda7c396ec4e1
David Docampo <david@docampo.ch>
2023-07-04 21:48:39 +0200

    added Work experience NavButton and reordered items

--- components/pages/index/TitleNav.vue
+++ components/pages/index/TitleNav.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="w-full sm:w-2/3 z-50 grid grid-cols-1 md:grid-cols-2 gap-6">
+    <NavButton text="Education" to="/education" class="w-full" />
+    <NavButton text="Work experience" to="/work-experience" class="w-full" />
     <NavButton text="LinkedIn" to="https://www.linkedin.com/in/david-docampo-47a9581a2/" target="_blank" class="w-full" />
     <NavButton text="GitHub" to="https://github.com/SayHeyD" target="_blank" class="w-full" />
-    <NavButton text="Education" to="/education" class="w-full" />
   </div>
 </template>
 
